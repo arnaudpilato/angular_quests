a7ff32fa880595b489303facc8f3a0838259dce9
Pil <70975556+arnaudpilato@users.noreply.github.com>
2021-12-22 11:03:42 +0100

    create challenge

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,30 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from "@angular/router";
 
 import { AppComponent } from './app.component';
 import { HighlightDirective } from './highlight.directive';
 import { ShowMoviesDirective } from './show-movies.directive';
 import { CustomDirectivesComponent } from './custom-directives/custom-directives.component';
+import { MenuComponent } from './menu/menu.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
+import { UserProfileComponent } from './user-profile/user-profile.component';
+
+import { ROUTES } from './app.routes';
 
 @NgModule({
   declarations: [
     AppComponent,
     HighlightDirective,
     ShowMoviesDirective,
-    CustomDirectivesComponent
+    CustomDirectivesComponent,
+    MenuComponent,
+    SignUpComponent,
+    UserProfileComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(ROUTES)
   ],
   providers: [],
   bootstrap: [AppComponent]
