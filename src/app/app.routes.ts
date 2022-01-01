533026c9889a74cacbe63eaf9b4b19eb5a15ab3d
Pil <70975556+arnaudpilato@users.noreply.github.com>
2022-01-01 22:19:11 +0100

    add challenge 03.2

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,13 @@
 import { Routes } from '@angular/router';
 import {SignUpComponent} from "./sign-up/sign-up.component";
 import {UserProfileComponent} from "./user-profile/user-profile.component";
+import {CustomDirectivesComponent} from "./custom-directives/custom-directives.component";
 
 const ROUTES: Routes = [
   { path: '', component: UserProfileComponent},
   { path: 'user', component: UserProfileComponent},
   { path: 'signup', component: SignUpComponent},
+  { path: 'custom-directives', component: CustomDirectivesComponent},
 
 ]
 
